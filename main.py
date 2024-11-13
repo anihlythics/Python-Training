a0f383dda1ff2ed32bbb897267a81406cd270f81
John Chibuzor Anih <anihcj57@gmail.com>
2024-11-13 21:34:59 +0000

    Nesting_conditions practise_02
    Retail service discounting terms

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-user_decide = input("Hello, would you like some movie recommendation tonight? ").lower()
+product_1 = int(input("What is the cost of product_A? "))
+product_2 = int(input("What is the cost of product_B? "))
+product_3 = int(input("What is the cost of product_C? "))
 
-if user_decide == "yes":
-    print("Lovely, make a choice from the next prompt.")
+total = product_1 + product_2 + product_3
 
-    movie_choice = input("Which movie genre would you prefer to watch from comedy, action or thriller? ").lower()
-    
-    if movie_choice == "comedy":
-        print("For tonight we would recommend the movie, Hangover Trilogy!, enjoy")
-    elif movie_choice == "action":
-        print("You might enjoy some packed action like Top Gun!, enjoy")
-    elif movie_choice == "thriller":
-        print("A great thriller choice would be Inception!, enjoy")
-    else:
-        print("Sorry, only pick from the available genre.")
-else:
-    print("Looks like you are tired tonight, good night")
+if product_1 < product_2 and product_2 < product_3:
+    total_cost = total * 0.5
+    print("Congrats you have a %50 discount on your purchase!", total_cost)
+
+if product_1 > product_2 and product_2 > product_3:
+    total_cost = total * 0.66
+    print("Congrats you have a 66% discount on your purchase!", total_cost)
