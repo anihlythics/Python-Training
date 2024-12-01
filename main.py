ee2b8084f4266ced577297e9cfff0c4bf66b60fe
John Chibuzor Anih <anihcj57@gmail.com>
2024-12-01 23:04:39 +0000

    python learning
    For condition learning, which introduces other attributes such as range and break.

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
-# counter = 0
+# username = input("Type in your code: ")
 
-# while counter < 3:
+# invalid = "0123"
+
+# for number in username:
+#     if number in invalid:
+#         print("This character is not allowed:", number)
+
+
+
+# vowels = "aeiou"
+
+# const = "bcdfghijklmnpqrstvwxyz"
+
+# word = input("Enter a word: ")
+
+# vowel_num = 0
+# for letter in word:
+#     if letter in vowels:
+#         vowel_num += 1 
+#     elif letter in const:
+#         vowel_num += 0
+
+# print("There are", vowel_num, "vowels in the word")
+
+
+# phone_number = input("Type in your phone number: ")
+
+# valid = "0123456789+"
+
+# for number in  phone_number:
+#     if number not in valid:
+#         print("Not a valid number")
+
+
+# guest = int(input("Enter number of guest: "))
+
+# for i in range(guest):
 #     name = input("Enter your name: ")
-#     print("Congrats!",name,"You saved 20%")
-#     counter += 1
-# print("All done")
-     
-
-tries = 0 
-
-code = ""
-
-while tries < 5 and code != 'python':
-    code = input("Enter a programming language: ")
-    tries += 1
-
-if code == 'python':
-    print("It took you",tries,"number of tries")
+#     age = int(input("Enter your age: "))
+    
+#     if age >= 18:
+#         print("Welcome,",name)
+#     else:
+#         # print("House rules no below 18 drinking, thanks.")
 
 
+
+for i in range(5):
+    username = input("Enter your username:")
+    pass_code = int(input("Enter your passcode: "))
+    
+    if username == "admin" or "pass_code" == "012345":
+        print("Welcome, Admin!")  
+        break
+        
+    if username != "admin" or pass_code != "012345":
+        print("You are not the admin")
